28b5b187bc452c016f387a433b4cad35db9145f9
laicommadarren <laicommadarren@gmail.com>
2023-06-26 20:13:17 -0700

    updates

---
+++ Lai_Darren_Portfolio/script.js
@@ -0,0 +1,11 @@
+function email() {
+  window.open("");
+}
+
+function linkedin() {
+  window.open("https://www.linkedin.com/in/laicommadarren/", "_blank");
+}
+
+function github() {
+  window.open("https://www.github.com/laicommadarren");
+}
